378ef357665f7ccdd0ad93e99c0c07972e19154b
GigaDeviceSemiconductor <gd32@gigadevice.com>
2026-08-16 11:41:24 +0800

    Update Firmware to V2.0.2

---
+++ Examples/TLI/TLI_2layers/gd32a490_libopt.h
@@ -0,0 +1,68 @@
+/*!
+    \file    gd32a490_libopt.h
+    \brief   library optional for gd32a490
+
+    \version 2023-11-30, V1.1.0 firmware for GD32A490
+*/
+
+/*
+    Copyright (c) 2025, GigaDevice Semiconductor Inc.
+
+    Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+
+    1. Redistributions of source code must retain the above copyright notice, this
+       list of conditions and the following disclaimer.
+    2. Redistributions in binary form must reproduce the above copyright notice,
+       this list of conditions and the following disclaimer in the documentation
+       and/or other materials provided with the distribution.
+    3. Neither the name of the copyright holder nor the names of its contributors
+       may be used to endorse or promote products derived from this software without
+       specific prior written permission.
+
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY
+OF SUCH DAMAGE.
+*/
+
+#ifndef GD32A490_LIBOPT_H
+#define GD32A490_LIBOPT_H
+
+#include "gd32a490_rcu.h"
+#include "gd32a490_adc.h"
+#include "gd32a490_can.h"
+#include "gd32a490_crc.h"
+#include "gd32a490_ctc.h"
+#include "gd32a490_dac.h"
+#include "gd32a490_dbg.h"
+#include "gd32a490_dci.h"
+#include "gd32a490_dma.h"
+#include "gd32a490_exti.h"
+#include "gd32a490_fmc.h"
+#include "gd32a490_fwdgt.h"
+#include "gd32a490_gpio.h"
+#include "gd32a490_syscfg.h"
+#include "gd32a490_i2c.h"
+#include "gd32a490_iref.h"
+#include "gd32a490_pmu.h"
+#include "gd32a490_rtc.h"
+#include "gd32a490_sdio.h"
+#include "gd32a490_spi.h"
+#include "gd32a490_timer.h"
+#include "gd32a490_trng.h"
+#include "gd32a490_usart.h"
+#include "gd32a490_wwdgt.h"
+#include "gd32a490_misc.h"
+#include "gd32a490_enet.h"
+#include "gd32a490_exmc.h"
+#include "gd32a490_ipa.h"
+#include "gd32a490_tli.h"
+
+#endif /* GD32A490_LIBOPT_H */
